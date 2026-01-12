826d6d0f4fed33be66fc9e94a98080889a904481
Smaranika Sahu <smaranika.s2006@gmail.com>
2026-01-12 10:13:56 +0000

    Initialize flask

---
+++ backend/routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify
+import json
+
+# from db import get_conn
+# from ml_model import predict_scores, vectorizer, model
+
+bp = Blueprint("api", __name__)
+
+@bp.get("/health")
+def health():
+    return {"status": "Server is running"}
+
